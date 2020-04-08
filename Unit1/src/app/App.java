a48df4183c8baff3823f43daf9b1de4986f0f8a2
gracesime <821324@mystma.org>
2020-04-08 16:34:11 +0000

    Added top five movies

---
+++ Unit1/src/app/App.java
@@ -0,0 +1,11 @@
+package app;
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        System.out.println("1. Harry Potter Series");
+        System.out.println("2. The Call of the Wild");
+        System.out.println("3. Black Panther");
+        System.out.println("4. Star Wars Series");
+        System.out.println("5. Lord of the Rings Trilogy");
+    }
+}
