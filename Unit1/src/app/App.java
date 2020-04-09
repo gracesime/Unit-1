9ef97f9a005a52a5d08fd2bb68a0e7c04b90c9ee
gracesime <821324@mystma.org>
2020-04-09 16:40:04 +0000

    variables and data types

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,10 +2,32 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("1. Harry Potter Series");
-        System.out.println("2. The Call of the Wild");
-        System.out.println("3. Black Panther");
-        System.out.println("4. Star Wars Series");
-        System.out.println("5. Lord of the Rings Trilogy");
+        boolean haveADog = true; 
+        byte tvsInHouse = 3; 
+        char lastLettterOfAlph = 'z'; 
+        short daysInYear = 365;
+        int populationOfMinnesota = 5611000;
+        long worldPopulation = 7800000000L;
+        float pennyWorth = 0.01f;
+        double massOfElectronInKg = 9.10938356e-31d; 
+
+
+        System.out.println("Mass of an electron in Kg");
+        System.out.println(massOfElectronInKg);
+        System.out.println("Worth of a Penny:");
+        System.out.println(pennyWorth);
+        System.out.println("World population:");
+        System.out.println(worldPopulation);
+        System.out.println("Population of MN:");
+        System.out.println(populationOfMinnesota);
+        System.out.println("Days in a year:");
+        System.out.println(daysInYear);
+        System.out.println("The last letter of the alphabet is:");
+        System.out.println(lastLettterOfAlph);
+        System.out.println("How many TVs are in my house?");
+        System.out.println(tvsInHouse);
+        System.out.println("Do I have a dog?");
+        System.out.println(haveADog);
+        
     }
 }
