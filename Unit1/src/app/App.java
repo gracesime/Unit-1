38951c47c46d3fd52f645645ead6227d06f6f1e6
gracesime <821324@mystma.org>
2020-04-13 16:01:14 +0000

    madlib

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,32 +2,23 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        boolean haveADog = true; 
-        byte tvsInHouse = 3; 
-        char lastLettterOfAlph = 'z'; 
-        short daysInYear = 365;
-        int populationOfMinnesota = 5611000;
-        long worldPopulation = 7800000000L;
-        float pennyWorth = 0.01f;
-        double massOfElectronInKg = 9.10938356e-31d; 
+        String person1 = "John";
+        String person2 = "Josh";
+        String verb1 = "drive";
+        String verb2 = "run";
+        String verb3 = "talk";
+        String food = "waffles";
+        String dayOfWeek = "saturday";
+        String place1 = "park";
+        String place2 = "lake";
+        String adjective1 = "fun";
+        String adjective2 = "relaxing";
 
-
-        System.out.println("Mass of an electron in Kg");
-        System.out.println(massOfElectronInKg);
-        System.out.println("Worth of a Penny:");
-        System.out.println(pennyWorth);
-        System.out.println("World population:");
-        System.out.println(worldPopulation);
-        System.out.println("Population of MN:");
-        System.out.println(populationOfMinnesota);
-        System.out.println("Days in a year:");
-        System.out.println(daysInYear);
-        System.out.println("The last letter of the alphabet is:");
-        System.out.println(lastLettterOfAlph);
-        System.out.println("How many TVs are in my house?");
-        System.out.println(tvsInHouse);
-        System.out.println("Do I have a dog?");
-        System.out.println(haveADog);
+        System.out.println(person1 + " " + "and" + " " + person2 + " " + "are freinds");
+        System.out.println("Every" + " " + dayOfWeek + " " + "they" + " " + verb1 + " " + "to the" + " " + place1);
+        System.out.println("Then they" + " " + verb2 + " " + "to the" + " " + place2 + " " + "and back");
+        System.out.println("Afterwards they get" + " " + food + " " + "and" + " " + verb3);
+        System.out.println("It is a" + " " + adjective1 + " " + "and" + " " + adjective2 + " " + "day");
         
     }
 }
