f80baa24efa50f859f472f6b3ef61419b3de1a8d
gracesime <821324@mystma.org>
2020-04-14 15:36:04 +0000

    while loops

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -2,23 +2,33 @@ package app;
 
 public class App {
     public static void main(String[] args) throws Exception {
-        String person1 = "John";
-        String person2 = "Josh";
-        String verb1 = "drive";
-        String verb2 = "run";
-        String verb3 = "talk";
-        String food = "waffles";
-        String dayOfWeek = "saturday";
-        String place1 = "park";
-        String place2 = "lake";
-        String adjective1 = "fun";
-        String adjective2 = "relaxing";
+        int count = 0;
+        String color = "red";
+        boolean mnIsCold = true; 
 
-        System.out.println(person1 + " " + "and" + " " + person2 + " " + "are freinds");
-        System.out.println("Every" + " " + dayOfWeek + " " + "they" + " " + verb1 + " " + "to the" + " " + place1);
-        System.out.println("Then they" + " " + verb2 + " " + "to the" + " " + place2 + " " + "and back");
-        System.out.println("Afterwards they get" + " " + food + " " + "and" + " " + verb3);
-        System.out.println("It is a" + " " + adjective1 + " " + "and" + " " + adjective2 + " " + "day");
-        
-    }
+
+        while (count <= 10) {
+            System.out.println("The count is " + count);
+            count = count + 1;
+        }
+
+        System.out.println("count finished");
+
+        while (color == "red"){
+            System.out.println("The color is " + color);
+            color = "green";
+        }
+
+        System.out.println("color finished");
+
+        while (mnIsCold == true) {
+            System.out.println("Minnesota is cold: " + mnIsCold);
+            mnIsCold = false;
+        }
+
+        System.out.println("mnIsCold finished");
+
+
+    
+    } 
 }
