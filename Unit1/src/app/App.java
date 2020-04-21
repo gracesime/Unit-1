51b9d9b014d331faf070a602625163aaf91eba3f
gracesime <821324@mystma.org>
2020-04-21 15:05:16 +0000

    more if statements

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -3,14 +3,30 @@ package app;
 public class App {
     public static void main(String[] args) throws Exception {
 
-        for (int i = 1; i <= 20; i++) {
-            if (i % 4 == 0) {
-                System.out.println(i + " is divisible by four");
+        System.out.println("Start first loop");
+
+        for (int i = 0; i <= 15; i++) {
+            if (i <= 9) {
+                System.out.println(i);
             } else {
-                System.out.println(i + " is not divisible by four");
+                System.out.println(10);
+            }
+        }
+        for (int i = 11; i <= 19; i++) {
+            System.out.println(i);
+        }
+
+        System.out.println("End first loop");
+        System.out.println("Start second loop");
+
+        for (int i = -1; i <= 19; i++){
+            if (i % 3 == 0){
+            }
+            else {
+                System.out.println(i);
             }
         }
 
-        System.out.println("All finished");
+        System.out.println("End second loop");
     }
 }
