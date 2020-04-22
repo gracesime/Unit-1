142962add075fb535aa7e218cca41c1fd4f5f841
gracesime <821324@mystma.org>
2020-04-22 17:19:43 +0000

    logic challenge

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -5,25 +5,23 @@ public class App {
 
         System.out.println("Start first loop");
 
-        for (int i = 0; i <= 19; i++) {
+        for (int i = 20; i >= 1; i = i - 1) {
             System.out.println(i);
             if (i == 10){
-                for (int j = 0; j < 5; j++){
+                for (int j = 0; j < 7; j++){
                     System.out.println(i);
                 }
             } 
         }
-
         System.out.println("End first loop");
         System.out.println("Start second loop");
-
-        for (int i = -1; i <= 19; i++){
-            if (i % 3 == 0){
-            }
-            else {
-                System.out.println(i);
-            }
+        
+        for (int i = 0; i <= 36; i = i + 4){
+            System.out.println(i); 
+             if (i % 6 == 0) {
+                System.out.println("Hey!");   
         }
+    }
 
         System.out.println("End second loop");
     }
