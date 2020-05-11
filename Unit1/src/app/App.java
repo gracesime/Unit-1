a1d915447153e6823cb419df7304ce893f7bb7b9
gracesime <821324@mystma.org>
2020-05-11 13:36:29 +0000

    calculator

--- Unit1/src/app/App.java
+++ Unit1/src/app/App.java
@@ -6,42 +6,28 @@ public class App {
 
         Scanner input = new Scanner(System.in);
 
-        System.out.println("Please enter a name.");
-        String name1 = input.nextLine();
+        System.out.println("Please enter the first number.");
+        int number1 = input.nextInt();
 
-        System.out.println("Please enter a name.");
-        String name2 = input.nextLine();
+        System.out.println("Please enter the second number.");
+        int number2 = input.nextInt();
 
-        System.out.println("Please enter an verb.");
-        String verb1 = input.nextLine();
+        System.out.println("Press 1 to add, 2 to subtract, 3 to multiply, or 4 to divide.");
+        int operation = input.nextInt();
+        if (operation == 1){
+            System.out.println(number1 + " + " + number2 + " = " + (number1 + number2));
+        }
+        if (operation == 2){
+            System.out.println(number1 + " - " + number2 + " = " + (number1 + number2));
+        }
+        if (operation == 3){
+            System.out.println(number1 + " * " + number2 + " = "+ (number1 * number2));
+        }
+        if (operation == 4){
+            System.out.println(number1 + " / " + number2 + " = " + (number1 / number2));
+        }
+       
 
-        System.out.println("Please enter a place.");
-        String place1 = input.nextLine();
-
-        System.out.println("Please enter an adjective");
-        String adjective1 = input.nextLine();
-
-        System.out.println("Please enter a noun.");
-        String noun1 = input.nextLine();
-
-        System.out.println("Please enter a past-tense verb.");
-        String verb2 = input.nextLine();
-
-        System.out.println("Please enter a verb.");
-        String verb3 = input.nextLine();
-
-        System.out.println("Please enter a food");
-        String food1 = input.nextLine();
-
-        System.out.println("Please enter an adjective");
-        String adjective2 = input.nextLine();
-        
-        System.out.println("One day, " + name1 + " and " + name2 + " were bored.");
-        System.out.println("They decided to " + verb1 + " to the " + place1 + ".");
-        System.out.println("When they got there, they saw a " + adjective1 + " " + noun1 + ".");
-        System.out.println("They went to get a closer look, but it " + verb2 + ".");
-        System.out.println("After that experience, they decided to " + verb3 + " home.");
-        System.out.println("Once home, they ate " + food1 + " while reflecting on a very " + adjective2 + " day.");
 
 
         
